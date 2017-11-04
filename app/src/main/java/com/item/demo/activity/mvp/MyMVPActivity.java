e25c61d3964564362c87be7eef58b83cc2b99fb4
wuzongjie <1825492258@qq.com>
2017-11-04 17:24:44 +0800

    添加一个类

---
+++ app/src/main/java/com/item/demo/activity/mvp/MyMVPActivity.java
@@ -0,0 +1,18 @@
+package com.item.demo.activity.mvp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.item.demo.R;
+
+/**
+ * MVP架构的体验
+ */
+public class MyMVPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_mvp);
+    }
+}
